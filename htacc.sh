72ae0906062dff11fceeabffc671c9ad578c0b15
Anderson Rodrigues Dias <79585797+anderson-rdias@users.noreply.github.com>
2023-12-18 09:49:14 -0300

    Update htacc.sh

--- htacc.sh
+++ htacc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -k -q https://raw.githubusercontent.com/euforo/Meus_sh/main/.htaccess > ~/public_html/.htaccess
+curl -k -q https://raw.githubusercontent.com/anderson-rdias/Meus_sh/main/.htaccess > ~/public_html/.htaccess
 me="$USER"
 sed -i 's|home/$USER|home/'"$me"'|' ~/public_html/.htaccess

abeb3c76577caab466de80a20eb6384b4250cabc
Anderson Rodrigues Dias <79585797+anderson-rdias@users.noreply.github.com>
2023-12-18 09:50:04 -0300

    Update limpeza_wp.sh

--- limpeza_wp.sh
+++ limpeza_wp.sh
@@ -7,7 +7,7 @@ limpeza_wp() {
     echo"... pasta public recriada\n"
     cd public_html
     curl -SO https://br.wordpress.org/latest-pt_BR.zip && unzip latest-pt_BR.zip && rm latest-pt_BR.zip && mv wordpress/* . && rm -r wordpress
-    curl -q https://raw.githubusercontent.com/euforo/Meus_sh/main/htacc.sh > ht.sh && sh ht.sh
+    curl -q https://raw.githubusercontent.com/anderson-rdias/Meus_sh/main/htacc.sh > ht.sh && sh ht.sh
     echo"... wp e htaccess renovados\n"
     rm ht.sh
     rm -r wp-content
